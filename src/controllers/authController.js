c26ebc423aa0cd29ade19bec706a80950a6cd9a3
mahame Alfred <mahamealfred@gmail.com>
2023-03-02 18:21:22 +0200

    login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,17 +1,20 @@
-const { model } = require("mongoose");
+const {decode, encode} =require('../helpers/jwtTokenizer')
 const User=require("../models/User");
+const dotenv=require('dotenv')
+const bcrypt=require('bcrypt')
+dotenv.config();
 
 
 class userController{
-    static async signIn(req, res) {
-     
-    }
+  
     static async signUp(req, res) {
       const {telephone,email,password}=req.body
       try {
          const checkTeacher=await User.findOne({teacherId:telephone});
          const checkParent=await User.findOne({parentId:telephone});
          const checkEmail=await  User.findOne({email:email})
+          const salt = await bcrypt.genSaltSync(10);
+          const hashedPassword = await bcrypt.hashSync(password, salt);
          if(checkEmail){
              return res.status(200).json({
                  statusCode: 400,
@@ -19,12 +22,10 @@ class userController{
                });  
          }
          else{
-            let dataUser
              if(checkTeacher){
-               dataUser = await User.updateOne(
+               await User.updateOne(
                   {"teacherId":telephone},
-                  {$set: { email:email,password:password}}); 
-
+                  {$set: { email:email,password:hashedPassword }}); 
                return res.status(200).json({
                  statusCode: 200,
                  status:"SUCCESS",
@@ -32,9 +33,9 @@ class userController{
                 
                });
              }else if(checkParent){
-               dataUser= await User.updateOne(
+             await User.updateOne(
                   {"parentId":telephone},
-                  {$set: { email:email,password:password}}); 
+                  {$set: { email:email,password:hashedPassword }}); 
                return res.status(200).json({
                  statusCode: 200,
                  status:"SUCCESS",
@@ -48,8 +49,6 @@ class userController{
                   message: "The Phone number bellow doesn't exist",
                 });  
              }
-              
-
          }
          
      } catch (error) {
@@ -60,6 +59,60 @@ class userController{
            });  
      }
      }
+
+     static async signIn(req, res) {
+      try {
+        const { email, password } = req.body;
+        const checkUser=await User.findOne({email:email})
+        if (!checkUser) {
+          return res.status(404).json({
+            status: 404,
+            message: "Email doesn't exist",
+          });
+        }
+        const dbEmail = checkUser.email;
+        const dbPasword = checkUser.password;
+        const telephone = checkUser.telephone;
+        const role = checkUser.role;
+        const parentId=checkUser.parentId
+        const teacherId=checkUser.teacherId
+        const decreptedPassword = await bcrypt.compare(password, dbPasword);
+          if (dbEmail === email) {
+            if (decreptedPassword) {
+              const token = await encode({
+                email,
+                telephone,
+                role,
+              });
+              //const decodedToken = await decode(token);
+              return res.status(200).json({
+               statusCode: 200,
+               status:"SUCCESS",
+                message: "Successfull logged",
+                data:{
+                  role,
+                  email,
+                  teacherId,
+                  parentId,
+                  token
+               },
+              });
+            }
+          }
+          return res.status(200).json({
+            statusCode: 400,
+            status:"FAILED",
+            message: "Password is not correct",
+          });
+     
+      } catch (error) {
+         return res.status(500).json({
+            statusCode: 500,
+            status:"FAILED",
+            message: error.message,
+          });  
+      }
+    }
 
 }
 module.exports =userController
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authController=require("../controllers/authController");
 const router=express.Router()
-router.post('/',authController.signIn)
+router.post('/sign-in',authController.signIn)
 router.post('/signup',authController.signUp)
 module.exports =router
